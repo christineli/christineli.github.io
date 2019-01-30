68fd2a7f61b164dcb9483d860f8f7593e783b339
Christine Li <christineli@Christines-MacBook-Pro.local>
2019-01-29 20:04:21 -0800

    Stylish tabs

--- opentab.js
+++ opentab.js
@@ -1,8 +1,14 @@
-function openTab(tabName) {
+function openTab(tabName, element) {
   var i;
   var x = document.getElementsByClassName("img-container");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
-  document.getElementById(tabName).style.display = "block";  
+  var tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.borderColor = "";
+  }
+  document.getElementById(tabName).style.display = "block";
+  element.style.borderColor = "#333";
+  
 }
