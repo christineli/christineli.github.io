a2f6d9cbc8087bf7024af0cfd1a989c98c3d9057
Christine Li <heyitschristineli@gmail.com>
2019-03-22 15:04:27 -0700

    Cleaned up repo

--- resize_images.sh
+++ resize_images.sh
@@ -1,11 +1,7 @@
-<<<<<<< Updated upstream
-photos="photos/*"
-for file in $travel;
-=======
-photos="media/*/*"
-for file in $photos;
->>>>>>> Stashed changes
+images="media/*"
+for file in $images;
 do
+  mv "$file" `echo $file | tr ' ' '_'` ;
   mv "$file" `echo $file | tr 'JPG' 'jpg'` ;
   convert $file -resize 1800x1800 $file
 done
